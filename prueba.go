016222f2d4c340ebb3d39e5c640880dddb5ef7e0
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2023-01-21 14:06:58 -0500

    pruebass

---
+++ prueba.go
@@ -0,0 +1,21 @@
+package prueba_go
+
+import (
+	"errors"
+)
+
+type Persona struct {
+	Nombre string
+	Edad   int
+}
+
+func (p Persona) CalcularAñoNacimiento() (int, error) {
+	año := 2023 - p.Edad
+	if año < 1 {
+		return 0, errors.New("No naciste en el futuro")
+
+	} else {
+		return año, nil
+	}
+
+}
