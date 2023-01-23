e7aea772d0be875fd726f9e63c96abe6ddde3e0c
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2023-01-23 11:51:31 -0500

    agresgue tests

--- prueba.go
+++ prueba.go
@@ -2,6 +2,7 @@ package prueba_go
 
 import (
 	"errors"
+	"testing"
 )
 
 type Persona struct {
@@ -22,4 +23,19 @@ func (p Persona) CalcularAñoNacimiento() (int, error) {
 
 func (p *Persona) Agear() {
 	p.Edad++
+}
+
+func testAgearAges(t *testing.T) {
+	Nombre := "Yaul"
+	Edad := 28
+	NAge := 10 // Podria ser un rn mas adelante
+	p := Persona{Nombre: Nombre, Edad: Edad}
+	i := 0
+	for i < NAge {
+		p.Agear()
+		i++
+	}
+	if p.Edad != Edad+i {
+		t.Fatalf("No funciona el aging")
+	}
 }
